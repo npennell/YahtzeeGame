5b2446bee058a8568d70feb9624469ca2f0b475f
Nichol <npennell@uoguelph.ca>
2020-05-29 15:23:55 -0400

    Initial Commit

---
+++ YahtzeeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YahtzeeGame
+//
+//  Created by Nichol Pennell on 2020-05-29.
+//  Copyright © 2020 Nichol Pennell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
